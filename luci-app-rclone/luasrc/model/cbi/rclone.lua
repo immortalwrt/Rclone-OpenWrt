44d778c4eac828b0adea85a82cee6f2c433edd1b
ElonH <elonhhuang@gmail.com>
2020-01-16 16:03:44 +0800

    :children_crossing: display rclone address #4

--- luci-app-rclone/luasrc/model/cbi/rclone.lua
+++ luci-app-rclone/luasrc/model/cbi/rclone.lua
@@ -12,10 +12,23 @@ local running = (luci.sys.call('pidof rclone > /dev/null') == 0)
 local state_msg = ''
 local trport = uci:get('rclone', 'config', 'port')
 
+local triptype = uci.get('rclone', 'config', 'addr_type')
+local trip = ''
+
+if triptype == 'local' then
+    trip = uci.get('network', 'loopback', 'ipaddr')
+elseif triptype == 'lan' then
+    trip = uci.get('network', 'lan', 'ipaddr')
+else
+    trip = '[ip]'
+end
+
 if running then
     state_msg = '<b><font color="green">' .. translate('rclone running') .. '</font></b>'
+    address_msg = translate('rclone address') .. ' : http://' .. trip .. ':' .. trport .. '<br/> <br/>'
 else
     state_msg = '<b><font color="red">' .. translate('rclone not run') .. '</font></b>'
+    address_msg = ''
 end
 
 m =
@@ -23,15 +36,12 @@ m =
     'rclone',
     translate('Rclone'),
     translate('Rclone ("rsync for cloud storage") is a command line program to sync root/usr/bin and directories to and from different cloud storage providers.') ..
-        ' <br/> <br/> ' ..
-            translate('rclone state') ..
-                ' : ' ..
-                    state_msg ..
-                        '<br/> <br/>' ..
-                            translate('Installed Web Interface') ..
-                                '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="button" class="cbi-button" style="margin: 0 5px;" value=" ' ..
-                                    translate('Webui React') ..
-                                        " \" onclick=\"window.open('http://'+window.location.hostname+'/rclone-webui-react')\"/> <br/><br/>"
+        ' <br/> <br/> ' .. translate('rclone state') .. ' : ' .. state_msg .. '<br/> <br/>'
+        .. address_msg ..
+        translate('Installed Web Interface') ..
+        '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="button" class="cbi-button" style="margin: 0 5px;" value=" ' ..
+        translate('Webui React') ..
+        " \" onclick=\"window.open('http://'+window.location.hostname+'/rclone-webui-react')\"/> <br/><br/>"
 )
 
 s = m:section(TypedSection, 'global', translate('global'))
