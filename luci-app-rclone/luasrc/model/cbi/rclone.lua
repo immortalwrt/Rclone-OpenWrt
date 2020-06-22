ab20f4656a4e939d174c3303c8d9b65b185fb551
ElonH <elonhhuang@gmail.com>
2020-06-22 14:00:22 +0800

    fix(luci-app-rclone): typo fix

--- luci-app-rclone/luasrc/model/cbi/rclone.lua
+++ luci-app-rclone/luasrc/model/cbi/rclone.lua
@@ -82,7 +82,7 @@ m =
     Map(
     'rclone',
     translate('Rclone'),
-    translate('Rclone ("rsync for cloud storage") is a command line program to sync root/usr/bin and directories to and from different cloud storage providers.')
+    translate('Rclone ("rsync for cloud storage") is a command line program to sync root/usr/bin and directories to and from different cloud storage providers.') ..
         ' <br/> <br/> ' .. translate('rclone state') .. ' : ' .. state_msg .. '<br/> <br/>'
         .. address_msg
 )
